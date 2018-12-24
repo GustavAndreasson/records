b56df6bf6a061b84fadb44c578fd9c7a29417af3
Gustav Andreasson <gustav.andreasson@gmail.com>
2018-12-24 07:52:59 +0100

    Database read/write for Artist class

--- library/Artist.php
+++ library/Artist.php
@@ -14,8 +14,13 @@ class Artist {
         $this->groups = array();
         if (is_object($artist)) { // store new artist in database with data from discogs data
             $this->id = $artist->id;
-            $this->name = $artist->name;
-            $this->description = $artist->profile;
+            $this->name = preg_replace("/\s\([0-9]+\)/", "", $artist->name, );
+            if (isset($artist->profile)) {
+                $this->description = $artist->profile;
+            }
+            $sql = "insert into artists (id, name, description) values (?, ?, ?) on duplicate key update id=id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute(array($this->id, $this->name, $this->description));
             if (isset($artist->members)) {
                 foreach ($artist->members as $member) {
                     $this->members[] = [
@@ -24,6 +29,9 @@ class Artist {
                             "name" => preg_replace("/\s\([0-9]+\)/", "", $member->name)]),
                         "active" => $member->active
                     ];
+                    $sql = "insert into artist_members (artist_id, member_id, active) values (?, ?, ?)";
+                    $stmt = $this->conn->prepare($sql);
+                    $stmt->execute(array($this->id, $member->id, $member->active));   
                 }
             }
             if (isset($artist->groups)) {
@@ -34,6 +42,9 @@ class Artist {
                             "name" => preg_replace("/\s\([0-9]+\)/", "", $group->name)]),
                         "active" => $group->active
                     ];
+                    $sql = "insert into artist_members (artist_id, member_id, active) values (?, ?, ?)";
+                    $stmt = $this->conn->prepare($sql);
+                    $stmt->execute(array($group->id, $this->id, $member->active));   
                 }
             }
         } elseif (is_array($artist)) { // data already collected from the database 
@@ -53,7 +64,27 @@ class Artist {
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
             if ($result) {
                 $this->name = $result["name"];
-                //$this->description = $result["description"];
+                $this->description = $result["description"];
+            }
+            $sql = "select a.id, a.name, ag.active from artist_groups ag ";
+            $sql .= "inner join artists a on ag.artist_id = a.id ";
+            $sql .= "where ag.artist_id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(array($this->id));
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $this->members[$row["id"]] = [
+                    "artist" => new Artist($this->conn, ["id" => $row["id"], "name" => $row["name"]]),
+                    "active" => $row["active"]];
+            }
+            $sql = "select a.id, a.name, ag.active from artist_groups ag ";
+            $sql .= "inner join artists a on ag.artist_id = a.id ";
+            $sql .= "where ag.member_id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(array($this->id));
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $this->groups[$row["id"]] = [
+                    "artist" => new Artist($this->conn, ["id" => $row["id"], "name" => $row["name"]]),
+                    "active" => $row["active"]];
             }
         }
     }
